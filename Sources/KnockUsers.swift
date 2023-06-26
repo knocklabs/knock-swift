780499670fe5cbf177153e740f993e84898124e9
Diego <faraday.timid_0g@icloud.com>
2023-06-26 15:21:01 -0600

    bug fix in updateUser and remove parameter in getUser
    Both functions operate on the current user

--- Sources/KnockUsers.swift
+++ Sources/KnockUsers.swift
@@ -97,11 +97,11 @@ public extension Knock {
         }
     }
     
-    func getUser(userId: String, completionHandler: @escaping ((Result<User, Error>) -> Void)) {
+    func getUser(completionHandler: @escaping ((Result<User, Error>) -> Void)) {
         self.api.decodeFromGet(User.self, path: "/users/\(userId)", queryItems: nil, then: completionHandler)
     }
     
     func updateUser(user: User, completionHandler: @escaping ((Result<User, Error>) -> Void)) {
-        self.api.decodeFromPut(User.self, path: "/users/\(userId)/\(user.id)", body: user, then: completionHandler)
+        self.api.decodeFromPut(User.self, path: "/users/\(userId)", body: user, then: completionHandler)
     }
 }
