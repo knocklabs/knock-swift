1f63481c6ecfeab55525bcd2904749be428539a7
Diego <faraday.timid_0g@icloud.com>
2023-06-27 13:19:58 -0600

    bug fix for SwiftPM
    Fix target path specification to match sources root folder.
    
    Previously it was on the SwiftPM default (Sources/Knock) but now it’s on Sources.

--- Package.swift
+++ Package.swift
@@ -18,6 +18,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "Knock"),
+        .target(
+            name: "Knock",
+            path: "Sources"
+        ),
     ]
 )
