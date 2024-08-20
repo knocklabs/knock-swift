c171d67d4819c43863bdc73a7c219a738ee0ce11
Matt Gardner <mgardner@knock.app>
2024-08-20 10:52:07 -0600

    Pod fixes

--- Sources/Knock.swift
+++ Sources/Knock.swift
@@ -10,7 +10,7 @@ import OSLog
 
 // Knock client SDK.
 public class Knock {
-    internal static let clientVersion = "1.2.5"
+    internal static let clientVersion = "1.2.6"
     
     public static var shared: Knock = Knock()
     
